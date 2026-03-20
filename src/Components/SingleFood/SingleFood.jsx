684f5e8836ea5865a56e82c319b48f2d943ee7e7
farhan-al-shahrier-siam <farhanalshahriersiam@gmail.com>
2026-03-20 15:13:32 +0600

    Added a button to all the cards and made it responsive with some raw css

--- src/Components/SingleFood/SingleFood.jsx
+++ src/Components/SingleFood/SingleFood.jsx
@@ -7,6 +7,9 @@ const SingleFood = ({food}) => {
         <div className='food-card'>
             <img src={food.strMealThumb} alt="" />
             <h1>{food.strMeal}</h1>
+            <div className='card-btn'>
+                <button>Add to cart</button>
+            </div>
         </div>
     );
 };
